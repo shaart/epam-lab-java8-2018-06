c21bf6ccde0403588a702b1eb670a43c475a4591
Artur <imshaart@gmail.com>
2018-06-17 02:54:11 +0300

    streams.part2.exercise3 implemented

--- src/test/java/streams/part2/exercise/Exercise3.java
+++ src/test/java/streams/part2/exercise/Exercise3.java
@@ -1,8 +1,10 @@
 package streams.part2.exercise;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.Test;
 
 public class Exercise3 {
 
@@ -10,7 +12,13 @@ public class Exercise3 {
     public void createLimitedStringWithOddNumbersSeparatedBySpaces() {
         int countNumbers = 10;
 
-        String result = null;
+        final int INIT_VALUE = 1;
+        final int STEP = 2;
+        String result = IntStream
+                .iterate(INIT_VALUE, value -> value + STEP)
+                .limit(countNumbers)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
 
         assertEquals("1 3 5 7 9 11 13 15 17 19", result);
     }
@@ -19,7 +27,15 @@ public class Exercise3 {
     public void extractEvenNumberedCharactersToNewString() {
         String source = "abcdefghijklm";
 
-        String result = null;
+        char[] chars = source.toCharArray();
+        final int EVEN = 2;
+
+        String result = IntStream.range(0, chars.length)
+                                 .filter(index -> index % EVEN == 0)
+                                 .collect(StringBuilder::new,
+                                         (builder, index) -> builder.append(chars[index]),
+                                         StringBuilder::append)
+                                 .toString();
 
         assertEquals("acegikm", result);
     }
